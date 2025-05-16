9bbc02fe300768de2d56be5b484d031acee460a0
Arul Natarajan <Arul.Natarajan@rabobank.nl>
2025-05-16 08:53:41 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/spicy_tulip_pos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spicy_tulip_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spicy Tulip POS',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Spicy Tulip POS')),
+        body: Center(child: Text('Welcome to your POS system!')),
+      ),
+    );
+  }
+}
